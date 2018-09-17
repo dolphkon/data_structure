79fd2c51279cb9cc80dfb0c4e11b48205c276ff5
dolphkon <34410364+dolphkon@users.noreply.github.com>
2018-09-17 10:58:45 +0800

    Update FindGreatestSumOfSubArray.java

--- FindGreatestSumOfSubArray.java
+++ FindGreatestSumOfSubArray.java
@@ -13,7 +13,7 @@ public class FindGreatestSumOfSubArray {
         int max = 0;            //存储连续子元素和的最大值
         for (int i = 0; i < array.length; i++) {
             //核心部分，好好理解.
-            if(currentSum<=0){      //如过当前连续n项的和小于等于0,则没必要与后面的元素相加
+            if(currentSum<=0){      //如果当前连续n项的和小于等于0,则没必要与后面的元素相加
                 currentSum = array[i];      //currentSum重新赋值
             }else{
                 currentSum += array[i];     //如果currentSum的值大于0,则继续与后面的元素相加,
